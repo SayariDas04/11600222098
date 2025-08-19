be0ea5afadb24cbce1b91f5924a16d99bfe5fd3f
Sayari Das <sayaridas04@gmail.com>
2025-08-19 12:00:17 +0530

    Add files via upload

--- MicroService.java
+++ MicroService.java
@@ -1,1 +1,32 @@
-from fastAPI import fastAPI;
+import java.util.Scanner;
+
+public class MicroService {
+    public static void main(String[] args) {
+        try (Scanner sc = new Scanner(System.in)) {
+            int max_a = 0, max_b = 0;
+            System.out.println("Enter the number of houses: ");
+            int n = sc.nextInt();
+            int[] arr = new int[n];
+            System.out.println("Enter the amounts of the houses: ");
+            for (int i = 0; i < n; i++) {
+                arr[i] = sc.nextInt();
+            }
+            System.out.println("The values of the array: ");
+            for (int i = 0; i < n; i++) {
+                System.out.print(arr[i] + " ");
+            }
+            System.out.println();
+            for (int i = 0; i < n; i += 2) {
+                max_a += arr[i];
+            }
+            for (int i = 1; i < n; i += 2) {
+                max_b += arr[i];
+            }
+            if (max_a > max_b) {
+                System.out.println("The maximum loot is: " + max_a);
+            } else {
+                System.out.println("The maximum loot is: " + max_b);
+            }
+        }
+    }
+}
